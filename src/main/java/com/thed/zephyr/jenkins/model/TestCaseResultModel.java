0982ecbca8994a1498bec4ffe930e2d53cfb6c1d
Mohan Kumar <mohan.kumar@getzephyr.com>
2015-06-25 20:05:33 +0530

    Removed Soap API properties

--- src/main/java/com/thed/zephyr/jenkins/model/TestCaseResultModel.java
+++ src/main/java/com/thed/zephyr/jenkins/model/TestCaseResultModel.java
@@ -1,10 +1,7 @@
 package com.thed.zephyr.jenkins.model;
 
-import com.thed.service.soap.RemoteTestcase;
-
 public class TestCaseResultModel {
 
-	private RemoteTestcase remoteTestcase;
 	private Boolean passed;
 	private String testCase;
 	private String testCaseName;
@@ -23,14 +20,6 @@ public class TestCaseResultModel {
 
 	public void setTestCase(String testCase) {
 		this.testCase = testCase;
-	}
-
-	public RemoteTestcase getRemoteTestcase() {
-		return remoteTestcase;
-	}
-
-	public void setRemoteTestcase(RemoteTestcase remoteTestcase) {
-		this.remoteTestcase = remoteTestcase;
 	}
 
 	public Boolean getPassed() {
